bb398e1c7246b76cd1427a4235181571553add7a
Tomas <tleffew1994@gmail.com>
2018-03-25 14:13:54 -0600

    refactor

--- Models/user.js
+++ Models/user.js
@@ -1,33 +1,50 @@
+import bcrypt from 'bcrypt';
 export default (sequelize, DataTypes) => {
-  const User = sequelize.define('users', {
-    username: {
-      type: DataTypes.STRING,
-      unique: true,
-      validate: {
-        isAlphanumeric: {
-          args: true,
-          msg: 'The username can only contain letters and numbers',
+  const User = sequelize.define(
+    'users',
+    {
+      username: {
+        type: DataTypes.STRING,
+        unique: true,
+        validate: {
+          isAlphanumeric: {
+            args: true,
+            msg: 'The username can only contain letters and numbers',
+          },
+          len: {
+            args: [3, 25],
+            msg: 'The username needs to be between 3 and 25 characters long',
+          },
         },
-        len: {
-          args: [3, 25],
-          msg: 'The username needs to be between 3 and 25 characters long',
+      },
+      email: {
+        type: DataTypes.STRING,
+        unique: true,
+        validate: {
+          isEmail: {
+            args: true,
+            msg: 'Invalid email',
+          },
+        },
+      },
+      password: {
+        type: DataTypes.STRING,
+        validate: {
+          len: {
+            args: [5, 100],
+            msg: 'Password needs to be between 5 and 100 characters long',
+          },
         },
       },
     },
-    email: {
-      type: DataTypes.STRING,
-      unique: true,
-      validate: {
-        isEmail: {
-          args: true,
-          msg: 'Invalid email',
+    {
+      hooks: {
+        afterValidate: async user => {
+          user.password = await bcrypt.hash(user.password, 12);
         },
       },
     },
-    password: {
-      type: DataTypes.STRING,
-    },
-  });
+  );
 
   User.associate = models => {
     // M:M
