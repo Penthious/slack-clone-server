0d37a1da7f65c22f1c96e9d23fde07b2d3668d92
Tomas <tleffew1994@gmail.com>
2018-04-22 00:32:14 -0600

    added in direct messages

--- Schema/directMessage.js
+++ Schema/directMessage.js
@@ -3,14 +3,16 @@ export default `
     id: Int!
     sender: User!
     receiverId: Int!
+    created_at: String!
+    text: String!
   }
 
 
   type Query {
-    directMessages: [DirectMessage!]!
+    directMessages(teamId: Int!, otherUserId: Int!): [DirectMessage!]!
   }
 
   type Mutation {
-    createDirectMessage(receiverId: Int!, text: String!): Boolean!
+    createDirectMessage(receiverId: Int!, teamId: Int!, text: String!): Boolean!
   }
 `;
